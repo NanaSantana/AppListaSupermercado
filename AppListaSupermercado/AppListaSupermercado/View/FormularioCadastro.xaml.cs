1462435931e7509f7e3a6a06d3ac362871bfb8ad
Usuario <Usuario@CPD02-M12>
2022-06-13 08:15:04 -0300

    Adicionar arquivos de projeto.

---
+++ AppListaSupermercado/AppListaSupermercado/View/FormularioCadastro.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppListaSupermercado.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FormularioCadastro : ContentPage
+    {
+        public FormularioCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void CadastroProduto(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ListaProdutos());
+        }
+    }
+}
---
+++ AppListaSupermercado/AppListaSupermercado/View/ListaProdutos.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppListaSupermercado.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListaProdutos : ContentPage
+    {
+        public ListaProdutos()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_formProduto(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new FormularioCadastro());
+        }
+    }
+}
