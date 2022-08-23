d243d1197798502e0d5da5a5cf64d50ea241a622
Usuario <Usuario@CPD02-M02>
2022-08-23 12:28:19 -0300

    Comecaando colocar o crud :D

--- AppListaSupermercado/AppListaSupermercado/Helper/SQLiteDatabaseHelper.cs
+++ AppListaSupermercado/AppListaSupermercado/Helper/SQLiteDatabaseHelper.cs
@@ -9,6 +9,32 @@ namespace AppListaSupermercado.Helper
 {
     public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET NomeProduto=?, Quantidade=?, PrecoUnitario=?, PrecoTotal=? WHERE id= ?";
+            return _conn.QueryAsync<Produto>(sql, p.NomeProduto, p.Quantidade, p.PrecoUnitario, p.PrecoTotal, p.Id);
+
+        }
+
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+        //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
         public void insert(Produto p)
         {
 
--- AppListaSupermercado/AppListaSupermercado/View/ListaProdutos.xaml.cs
+++ AppListaSupermercado/AppListaSupermercado/View/ListaProdutos.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AppListaSupermercado.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +14,60 @@ namespace AppListaSupermercado.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListaProdutos : ContentPage
     {
+        ObservableCollection<Produto> lista_produtos = new ObservableCollection<Produto>();
         public ListaProdutos()
         {
             InitializeComponent();
+
+            lst_produtos.ItemsSource = lista_produtos;
         }
 
         private void Btn_formProduto(object sender, EventArgs e)
         {
             Navigation.PushAsync(new FormularioCadastro());
+        }
+
+        private void lts_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolbarItem_Clicked_Somar(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolbarItem_Clicked_Novo(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new NovoProduto());
+            } catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Ok");
+            }
+
+        }
+
+        private void txt_Busca_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        protected override void OnAppearing()
+        {
+            if (lista_produtos.Count == 0)
+            {
+                System.Threading.Tasks.Task.Run(async() =>
+                {
+                    List<Produto> temp = await App.Database.GetAll();
+                } //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            }
         }
     }
 }
